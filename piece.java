9182aaae1728d1965d678fa81b3138d588457348
Sean Steele <steele.sean303@gmail.com>
2013-05-09 08:47:21 -0600

    Made all of the pieces

--- piece.java
+++ piece.java
@@ -1,48 +1,20 @@
-import java.awt.Color;
-import java.awt.Image;
-
-import javax.swing.JFrame;
-
-public abstract class piece {
-
-	private Color color;
-	private int[][] position;
-	private Image picture;
-
-	public piece(Color color, int[][] position, Image picture) {
-    	this.color = color;
-    	this.position = position;
-    	this.picture = picture;
-  }
-
-	/********************************************
-   * 
-   * Getters and Setters
-   * 
-   *******************************************/
-
-  public Color getColor() {
-    return color;
-  }
-
-  public void setColor(Color color) {
-    this.color = color;
-  }
-
-  public int[][] getPosition() {
-    return position;
-  }
-
-  public void setPosition(int x, int y) {
-    this.position = new int[x][y];
-  }
-
-  public Image getPicture() {
-    return picture;
-  }
-
-  public void setPicture(Image picture) {
-    this.picture = picture;
-  }
+public abstract class Piece {
 	
+	public String color;
+	public boolean hasMoved;
+	public boolean ep_able;
+	
+	/**
+	 * Gets the color
+	 * @return color of the piee
+	 */
+	public abstract String getColor();
+	
+	/**
+	 * Moves the piece 
+	 * 
+	 * @return true if the move was valid or not.
+	 */
+	public abstract boolean validateMove(Piece[][] board, int currentRow, int currentCol, int newRow, int newCol);
+
 }
